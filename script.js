635502955447df3fb687b73bb8310f73ed6dcbf1
Bishar <bishar995@gmail.com>
2024-11-12 13:34:03 +0300

    added font family

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const sunEL = document.querySelector(".sun");
+const heroImgEl = document.querySelector(".hero-img_tag");
 
 sunEL.addEventListener("click", function () {
   document.body.classList.toggle("darkTheme");
   if (document.body.classList.contains("darkTheme")) {
     sunEL.src = "img/moon.png";
-  }else{
+    heroImgEl.src = "members/bashka.jpg";
+  } else {
     sunEL.src = "img/sun.png";
+    heroImgEl.src = "img/hero.jpg";
   }
 });
