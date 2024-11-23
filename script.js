092c5ca2849bdd5db5ce5cadf5fa6f2c060fb0ca
Bishar <bishar995@gmail.com>
2024-11-23 20:40:27 +0300

    ALLLmost

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ DateFun();
 sunEL.addEventListener("click", function () {
   document.body.classList.toggle("darkTheme");
   if (document.body.classList.contains("darkTheme")) {
+    sunEL.src = "img/moon.png";
   } else {
     sunEL.src = "img/sun.png";
   }
